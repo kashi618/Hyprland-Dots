cd344e13c51228102d298a507c92a5797862bfbe
Neil Jiang <nialj618@gmail.com>
2025-11-02 14:18:24 +0000

    sync

---
+++ Hyprland-Dots-Install-AUR.sh
@@ -0,0 +1,11 @@
+yay -S\
+ linux-wifi-hotspot\
+ hyprshot\
+ hyprswitch\
+ laptop-mode-tools\
+ cable\
+ protonup-qt\
+ steamtinkerlaunch\
+ visual-studio-code-bin\
+ github-desktop-bin\
+ notion-calendar-electron
