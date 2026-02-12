b83ca963cb952e8f281dfd8696f594614646e198
kashi <nialj618@gmail.com>
2026-02-12 15:35:02 +0000

    Changed some keybindings for Hyprland and Nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,22 +15,25 @@ vim.opt.termguicolors = true
 -- Enable persistant undo
 vim.opt.undofile = true
 
-
 -- Show invisible characters
-vim.opt.list = true
+vim.opt.list = ture
 
--- Map jk to esc
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap=true, silent=true})
+---------- Keybindings ----------
+-- Deindent using shift+tab
+vim.keymap.set('i', '<S-Tab>', '<C-d>', {noremap=true, silent=true})
+
+-- Map jj/jk to esc
+vim.keymap.set('i', 'jk', '<Esc>', {noremap=true, silent=true})
+vim.keymap.set('i', 'jj', '<Esc>', {noremap=true, silent=true})
 
 -- nvim-tree keybindings
 vim.keymap.set('n', 't', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle nvim-tree' })
 vim.keymap.set('n', 'T', '<cmd>NvimTreeFocus<CR>', { desc = 'Focus nvim-tree' })
 
 -- telescope keybindings
-vim.keymap.set('n', 'r', '<cmd>builtin.find_files<CR>', { desc = 'Telescope Find Files'} )
+vim.keymap.set('n', 'r', '<cmd>Telescope find_files<CR>', { desc = 'Telescope Find Files'} )
 vim.keymap.set('n', 'R', '<cmd>Telescope live_grep<CR>', { desc = 'Telescope Live Grep'} )
-vim.keymap.set('n', 'f', '<cmd>Telescope current_buffer_fuzzy_find', { desc = 'Telescope Frzzy Find'})
+vim.keymap.set('n', 'f', '<cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = 'Telescope Frzzy Find'})
 
 -- lazy.nvim
 require("config.lazy")
